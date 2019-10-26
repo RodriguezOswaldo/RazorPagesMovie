17d62b532b12fa394b1900f96b6d7826dc4ae4bc
Oswaldo Rodriguez <Owner2303@gmail.com>
2019-10-25 21:23:53 -0400

    Addding ratings field

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -16,7 +16,9 @@ namespace RazorPagesMovie.Models
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }  
+        public decimal Price { get; set; }
+
+        public string Rating { get; set; }
 
     } 
 }
